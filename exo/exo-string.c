ee38e182b99cbd755874eb1157ac06d0aaac2004
Benedikt Meurer <benny@xfce.org>
2007-05-22 16:15:32 +0000

    2007-05-22	Benedikt Meurer <benny@xfce.org>
    	* configure.in.in, exo/exo-string.{c,h}, exo/exo.symbols: Add new
    	  function exo_strdup_strftime(), which is an UTF-8 aware wrapper
    	  for strftime().
    	* docs/reference/: Update reference manual.
    
    
    
    
    (Old svn revision: 25740)

--- exo/exo-string.c
+++ exo/exo-string.c
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*-
- * Copyright (c) 2004-2005 os-cillation e.K.
+ * Copyright (c) 2004-2007 os-cillation e.K.
  *
  * Written by Benedikt Meurer <benny@xfce.org>.
  *
@@ -170,6 +170,187 @@ exo_str_replace (const gchar *str,
     }
 
   return g_string_free (result, FALSE);
+}
+
+
+
+/**
+ * exo_strdup_strftime:
+ * @format : format string to pass to strftime(3). See the strftime(3) documentation
+ *           for details.
+ * @tm     : date/time, in struct tm format.
+ *
+ * Cover for standard date-and-time-formatting routine strftime that returns
+ * a newly-allocated string of the correct size. The caller is responsible
+ * to free the returned string using g_free() when no longer needed.
+ *
+ * Besides the buffer management, there are two differences between this
+ * and the library strftime:
+ *
+ * The modifiers "-" and "_" between a "%" and a numeric directive
+ * are defined as for the GNU version of strftime. "-" means "do not
+ * pad the field" and "_" means "pad with spaces instead of zeroes".
+ *
+ * Non-ANSI extensions to strftime are flagged at runtime with a
+ * warning, so it's easy to notice use of the extensions without
+ * testing with multiple versions of the library.
+ *
+ * Return value: a newly allocated string containing the formatted date/time.
+ *
+ * Since: 0.3.3
+ **/
+gchar*
+exo_strdup_strftime (const gchar     *format,
+                     const struct tm *tm)
+{
+  static const gchar C_STANDARD_STRFTIME_CHARACTERS[] = "aAbBcdHIjmMpSUwWxXyYZ";
+  static const gchar C_STANDARD_NUMERIC_STRFTIME_CHARACTERS[] = "dHIjmMSUwWyY";
+  static const gchar SUS_EXTENDED_STRFTIME_MODIFIERS[] = "EO";
+  const gchar       *remainder;
+  const gchar       *percent;
+  gboolean           strip_leading_zeros;
+  gboolean           turn_leading_zeros_to_spaces;
+  GString           *string;
+  gsize              string_length;
+  gchar              code[4];
+  gchar              buffer[512];
+  gchar             *piece;
+  gchar             *result;
+  gchar             *converted;
+  gchar              modifier;
+  gint               i;
+
+  /* Format could be translated, and contain UTF-8 chars,
+   * so convert to locale encoding which strftime uses.
+   */
+  converted = g_locale_from_utf8 (format, -1, NULL, NULL, NULL);
+  if (G_UNLIKELY (converted == NULL))
+    return NULL;
+  
+  /* start processing the format */
+  string = g_string_new ("");
+  remainder = converted;
+
+  /* walk from % character to % character */
+  for (;;)
+    {
+      /* look out for the next % character */
+      percent = strchr (remainder, '%');
+      if (percent == NULL)
+        {
+          /* we're done with the format */
+          g_string_append (string, remainder);
+          break;
+        }
+
+      /* append the characters in between */
+      g_string_append_len (string, remainder, percent - remainder);
+
+      /* handle the "%" character */
+      remainder = percent + 1;
+      switch (*remainder)
+        {
+        case '-':
+          strip_leading_zeros = TRUE;
+          turn_leading_zeros_to_spaces = FALSE;
+          remainder++;
+          break;
+
+        case '_':
+          strip_leading_zeros = FALSE;
+          turn_leading_zeros_to_spaces = TRUE;
+          remainder++;
+          break;
+
+        case '%':
+          g_string_append_c (string, '%');
+          remainder++;
+          continue;
+
+        case '\0':
+          g_warning ("Trailing %% passed to exo_strdup_strftime");
+          g_string_append_c (string, '%');
+          continue;
+
+        default:
+          strip_leading_zeros = FALSE;
+          turn_leading_zeros_to_spaces = FALSE;
+          break;
+        }
+
+      modifier = 0;
+      if (strchr (SUS_EXTENDED_STRFTIME_MODIFIERS, *remainder) != NULL)
+        {
+          modifier = *remainder++;
+          if (*remainder == 0)
+            {
+              g_warning ("Unfinished %%%c modifier passed to exo_strdup_strftime", modifier);
+              break;
+            }
+        } 
+      
+      if (strchr (C_STANDARD_STRFTIME_CHARACTERS, *remainder) == NULL)
+        g_warning ("exo_strdup_strftime does not support non-standard escape code %%%c", *remainder);
+
+      /* convert code to strftime format. We have a fixed
+       * limit here that each code can expand to a maximum
+       * of 512 bytes, which is probably OK. There's no
+       * limit on the total size of the result string.
+       */
+      i = 0;
+      code[i++] = '%';
+#ifdef HAVE_STRFTIME_EXTENSION
+      if (modifier != 0)
+        code[i++] = modifier;
+#endif
+      code[i++] = *remainder;
+      code[i++] = '\0';
+      string_length = strftime (buffer, sizeof (buffer), code, tm);
+      if (string_length == 0)
+        {
+          /* we could put a warning here, but there's no
+           * way to tell a successful conversion to
+           * empty string from a failure
+           */
+          buffer[0] = '\0';
+        }
+
+      /* strip leading zeros if requested */
+      piece = buffer;
+      if (strip_leading_zeros || turn_leading_zeros_to_spaces)
+        {
+          if (strchr (C_STANDARD_NUMERIC_STRFTIME_CHARACTERS, *remainder) == NULL)
+            g_warning ("exo_strdup_strftime does not support modifier for non-numeric escape code %%%c%c", remainder[-1], *remainder);
+
+          if (*piece == '0')
+            {
+              while (*++piece == '\0') ;
+              if (!g_ascii_isdigit (*piece))
+                --piece;
+            }
+
+          if (turn_leading_zeros_to_spaces)
+            {
+              memset (buffer, ' ', piece - buffer);
+              piece = buffer;
+            }
+        }
+
+      /* advance */
+      ++remainder;
+
+      /* add this piece */
+      g_string_append (string, piece);
+    }
+  
+  /* Convert the string back into UTF-8 */
+  result = g_locale_to_utf8 (string->str, -1, NULL, NULL, NULL);
+
+  /* cleanup */
+  g_string_free (string, TRUE);
+  g_free (converted);
+
+  return result;
 }
 
 
