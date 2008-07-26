8ffc8bdf0c37b3a5114a91be4bb744333ce9881c
Jannis Pohlmann <jannis@xfce.org>
2008-07-26 12:27:03 +0000

    * autogen.sh: Add @REVISION@ substitution support for git svn 	  repositories.
    (Old svn revision: 27375)

--- autogen.sh
+++ autogen.sh
@@ -29,7 +29,14 @@ EOF
 
 # substitute revision and linguas
 linguas=`sed -e '/^#/d' po/LINGUAS`
-revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
+if [ -d .git/svn ]; then
+  revision=`git svn find-rev trunk ||
+            git svn find-rev origin/trunk ||
+            git svn find-rev HEAD ||
+            git svn find-rev master`
+else
+  revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
+fi
 sed -e "s/@LINGUAS@/${linguas}/g" \
     -e "s/@REVISION@/${revision}/g" \
     < "configure.in.in" > "configure.in"
