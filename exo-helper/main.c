2226463bdfa547f659fe5cd1c7520023d21a0879
Jannis Pohlmann <jannis@xfce.org>
2008-11-23 16:32:05 +0000

    * exo-helper/main.c: Oops, I actually broke the --launch parameter. Fixed.
    (Old svn revision: 28893)

--- exo-helper/main.c
+++ exo-helper/main.c
@@ -67,8 +67,8 @@ main (int argc, char **argv)
     { "version", 'V', G_OPTION_FLAG_IN_MAIN, G_OPTION_ARG_NONE, &opt_version, N_("Print version information and exit"), NULL, },
     { "configure", 'c', G_OPTION_FLAG_IN_MAIN, G_OPTION_ARG_NONE, &opt_configure, N_("Open the Preferred Applications\nconfiguration dialog"), NULL, },
     { "socket-id", 's', G_OPTION_FLAG_IN_MAIN, G_OPTION_ARG_INT, &opt_socket_id, N_("Settings manager socket"), N_("SOCKET ID"), },
-    { NULL, },
     { "launch", 'l', G_OPTION_FLAG_IN_MAIN, G_OPTION_ARG_STRING, &opt_launch_type, N_("Launch the default helper of TYPE with the optional PARAMETER, where TYPE is one of the following values."), N_("TYPE [PARAMETER]"), },
+    { NULL, },
   };
 
   /* sanity check helper categories */
