121bb931d3d161b2f1716fb3c0f0962492146988
Nick Schermer <nick@xfce.org>
2009-07-10 18:40:55 +0000

    Merge from trunk.
    (Old svn revision: 30253)

--- exo-open/main.c
+++ exo-open/main.c
@@ -37,6 +37,19 @@
 
 
 
+/**
+ * For testing this code the following commands should work:
+ * 
+ * exo-open --launch WebBrowser http://xfce.org (bug #5461).
+ * exo-open http://xfce.org
+ * exo-open --launch TerminalEmulator ./script.sh 'something with a space' 'nospace' (bug #5132).
+ * exo-open --launch TerminalEmulator ssh -l username some.host.com
+ * xfterm4 -e ssh -l ssh -l username some.host.com (bug #5301, this generates line below)
+ *   exo-open --launch TerminalEmulator 'ssh -l username some.host.com'
+ **/
+
+
+
 static gboolean opt_help = FALSE;
 static gboolean opt_version = FALSE;
 static gchar   *opt_launch = NULL;
@@ -143,6 +156,8 @@ main (int argc, char **argv)
     {
       if (argc > 1)
         {
+          /* NOTE: see the comment at the top of this document! */
+
           /* combine all specified parameters to one parameter string */
           join = g_string_new (NULL);
           for (i = 1; argv[i] != NULL; i++)
@@ -150,11 +165,20 @@ main (int argc, char **argv)
               /* separate the arguments */
               if (i > 1)
                 join = g_string_append_c (join, ' ');
-
-              /* append the quoted argument */
-              quoted = g_shell_quote (argv[i]);
-              join = g_string_append (join, quoted);
-              g_free (quoted);
+              
+              /* only quote arguments with spaces if there are multiple
+               * arguments to be merged, this is a bit of magic to make
+               * common cares work property, see sample above with xfrun4 */
+              if (argc > 2 && strchr (argv[i], ' ') != NULL)
+                {
+                  quoted = g_shell_quote (argv[i]);
+                  join = g_string_append (join, quoted);
+                  g_free (quoted);
+                }
+              else
+                {
+                  join = g_string_append (join, argv[i]);
+                }
             }
           parameter = g_string_free (join, FALSE);
         }
@@ -162,6 +186,10 @@ main (int argc, char **argv)
         {
           parameter = NULL;
         }
+
+#ifndef NDEBUG
+      g_message ("launch=%s, wd=%s, parameters (%d)=%s", opt_launch, opt_working_directory, argc, parameter);
+#endif
 
       /* run the preferred application */
       if (!exo_execute_preferred_application (opt_launch, parameter, opt_working_directory, NULL, &err))
