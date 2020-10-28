f3768cbb9d1e09fa81b729a104afcf4486a8d6d2
Alexander Schwinn <alexxcons@xfce.org>
2020-10-29 00:26:11 +0100

    Re-grab tree selection to prevent rare crash (Issue #46)
    After calling the parent button_press_event handler (gtk_tree_view) it
    is required to update the selection, since it seems like the parent
    button_press_event handler might leave the old selection invalid in some
    cases.
    
    Fixes #46

--- exo/exo-tree-view.c
+++ exo/exo-tree-view.c
@@ -355,8 +355,9 @@ exo_tree_view_button_press_event (GtkWidget      *widget,
   result = (*GTK_WIDGET_CLASS (exo_tree_view_parent_class)->button_press_event) (widget, event);
 
   /* Re-enable selection updates. Note that since we have already "chained up" by calling the parent's button press handler,
-   * we must check that the selection is still valid with GTK_IS_TREE_SELECTION.
-   */
+   * we must re-grab the tree selection, since the old one might be corrupted */
+  selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (tree_view));
+
   if (GTK_IS_TREE_SELECTION (selection) &&
       G_LIKELY (gtk_tree_selection_get_select_function (selection) == (GtkTreeSelectionFunc) (void (*)(void)) exo_noop_false))
     {
